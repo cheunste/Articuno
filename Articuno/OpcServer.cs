ed76b905e6a1f5c66c3eb431d9c0e6108e1e469b
Stephen <cheunste@gmail.com>
2019-05-28 13:01:06 -0700

    Added comments in the OpcServer class. HOwever, I stil have not implemented any of the three methods. All willl throw a NotImplementedException until I figure out the library

--- Articuno/OpcServer.cs
+++ Articuno/OpcServer.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace Articuno
 {
+    /// <summary>
+    /// The OpcServer class sets up an interface to the OpcServer (PcVue). This probalby should be a singleton as there should only be one instance of this
+    /// </summary>
     internal class OpcServer
     {
+        //Constructor. Tages in a server name and sets the 
         public OpcServer(String serverName)
         {
+            throw new NotImplementedException();
 
         }
-        //TODO: Implement
         /// <summary>
         /// The readTags function reads an OPC tag value given an OPC Tag. This always returns a String.
         /// Mainly because I don't trust what the server is returning. 
@@ -24,7 +28,26 @@ namespace Articuno
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Sets a value given  an OPC tagname and a value
+        /// </summary>
+        /// <param name="tag">the name of hte OPC tag</param>
+        /// <param name="value">the value to set to</param>
+        //This method is used to set the value of an OPC tag to some value. mainly used for the sensors in the met tower so it doesn't go out of range 
         public void setTag(string tag,Object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads several OPC tag given a string array of tagnames.
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns>An array  string format</returns>
+        //This method reads in an array of tagnames (in String) and returns an array of string values (relative to the tagname's position)
+        //It returns a string array as depending on the OPC tag, it could return a boolean, a text, or a double. Mind as well cast them all to a string
+        //to prevent parameter guessing 
+        public string[] readTags(string[] tagNames)
         {
             throw new NotImplementedException();
         }
