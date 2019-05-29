eb771f3f720a5e04afa4e084cad5102305cdfd71
Stephen <cheunste@gmail.com>
2019-05-29 14:30:35 -0700

    Created the functions in the Timer class

--- Articuno/Timer.cs
+++ Articuno/Timer.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace Articuno
 {
+    /// <summary>
+    /// The Timer class is responsible for handling all the timing criteria in the Articuno project, such as...
+    /// - CTR
+    /// - Sensor sampling intervals
+    /// - Setting flags signaling rest of program when to execute certain tasks
+    /// - Clearing the noted flags from above
+    /// 
+    /// </summary>
     class Timer
     {
+        private static bool minuteFlag;
+        private static bool ctrlFlag;
+
+
+        public Timer()
+        {
+
+        }
+
+        /// <summary>
+        /// function to check the time to see if certain time periods have passed
+        /// </summary>
+        /// <returns></returns>
+        public DateTime checkTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get the timer flag
+        /// </summary>
+        /// <returns></returns>
+        public bool getTimerFlag()
+        {
+            throw new NotImplementedException();
+        }
+        public void  setCtrTime()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void setCtfFlag()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void clearCtfFlag()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void ctrChange()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
