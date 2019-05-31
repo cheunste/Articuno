3b539961d453740edc13ebe03e9570a4c6da6a8b
Stephen <cheunste@gmail.com>
2019-05-31 08:03:32 -0700

    Declared most of the member variables and methods in the Turbine and TurbineFactory classes. Most of the methods will be throwing NotIMplementedExceptions()
    Had to modify the db so that it takes in a loadshutdown tag (for pausing)

--- Articuno/Turbine.cs
+++ Articuno/Turbine.cs
@@ -1,5 +1,6 @@
 ﻿using log4net;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,102 @@ using System.Threading.Tasks;
 
 namespace Articuno
 {
+    /// <summary>
+    /// The turbine class exposes OPC tags that are relevant to the program.
+    /// You can set the tags via the setXXXXXTag method and get the value of said tag via the getXXXXXXValue method
+    /// </summary>
     class Turbine
     {
+
+        //Member variables for OPC Tags in the turbine
+        private string turbinePrefix;
+        private string operatingState;
+        private string rotorSpeed;
+        private string nacelleWindSpeed;
+        private string deRate;
+        private string nrsState;
+        private string primMetReference;
+        private string secMetReference;
+        private string loadShutDown;
+
+        //Member variables for algorithm
+        private bool temperatureConditionMet;
+        private bool operatingStateConditionMet;
+        private bool nrsConditionMet;
+        private bool turbinePerformanceConditionMet;
+        private bool derateConditionMet;
+
+        private int currentTurbSP;
+        private Queue temperatureQueue;
+        private bool articunoParicipation;
+
+
+
+        //Log
         private static readonly ILog log = LogManager.GetLogger(typeof(Setup));
+
+        public Turbine(string prefix)
+        {
+
+        }
+
+        //Detects when operating state (run, pause, etc.) changes
+        public string operatingStateChanged() { throw new NotImplementedException(); }
+
+        //Get the value for the wind speed, rotor speed, etc. value from the OPC Server
+        public double getWindSpeedValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getrotorSpeedValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getOperatinStateValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getNrsStateValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getTemperatureValue(OpcServer server) { throw new NotImplementedException(); }
+        //Load shutdown is a bit different as it is a command
+        public double sentLoadShutdownCmd(OpcServer server) {
+            log.Info("Shutdown command for "+this.turbinePrefix + " has been sent");
+            throw new NotImplementedException();
+        }
+
+        //Sets the OPC tag to the class's  member variable
+        public void setWindSpeedTag(string tag) { throw new NotImplementedException(); }
+        public void setrotorSpeedTag(string tag) { throw new NotImplementedException(); }
+        public void setOperatinStateTag(string tag) { throw new NotImplementedException(); }
+        public void setNrsStateTag(string tag) { throw new NotImplementedException(); }
+        public void setTemperatureTag(string tag) { throw new NotImplementedException(); }
+        public void setLoadShutdownTag(string tag) { throw new NotImplementedException(); }
+
+        //The following five fucntions are set by the main Articuno class. They show if each of the four/five 
+        //algorithms are true
+        public void setTemperatureCondition(bool state) { this.temperatureConditionMet = state; }
+        public void setOperatingStateCondition(bool state) { this.operatingStateConditionMet = state; }
+        public void setNrsCondition(bool state) { this.nrsConditionMet = state; }
+        public void setTurbinePerformanceCondition(bool state) { turbinePerformanceConditionMet = state; }
+        public void setDeRateCondition(bool state) { derateConditionMet = state; }
+        
+        //Misc functions
+        public string getTurbinePrefix() { return this.turbinePrefix; }
+
+        public string getTurbineCTR() {   throw new NotImplementedException(); }
+
+        //Met Tower References for turbines. One sets the met tower reference (upon create) and the other gets it. 
+        //These can be set to another reference  if/when they fail
+        public void setMetReference() {    throw new NotImplementedException(); }
+        public int getMetReference() {    throw new NotImplementedException(); }
+
+        //Turbine set point getter and setter
+        public int getTurbineSP(){throw new NotImplementedException(); }
+        public void setTurbineSP(int setPoint){throw new NotImplementedException(); }
+
+        /*
+        This method is special. It adds the current temperature to a temperature queue. 
+        This method is also in the main Articuno class and
+        This is used for calculating CTR minute temperature averages based on averaging one minute averages, which CORE may or may not be providing
+        IF AND ONLY IF The met tower temperatures are NOT working. This is a used for turbines that are supposed to be met tower redundancy
+        The way it works is as follows
+         - you collect one minute average temperatures values every minute (This will be determined by your timer class)
+         - After CTR minute has passed (this could be 10, or 15, or 1, it is dependent on user), then 
+         - calculate the average temperature of all the one min temperature values in the entire queue
+         - perform a dequeue to remove the first most value
+        */
+        public void addToTemperatureQueue(double temperature) { throw new NotImplementedException(); } 
+
     }
 }
--- Articuno/TurbineFactory.cs
+++ Articuno/TurbineFactory.cs
@@ -7,8 +7,71 @@ using System.Threading.Tasks;
 
 namespace Articuno
 {
+    /// <summary>
+    /// The turbine FActory is a factory class responsible for creating the numerous turbines at the site.
+    /// It takes in a list of turbine prefixes and then creates a turbine class based  on the prefix
+    /// The factory class also have other methods that'll allow interfaction with all the turbines in its turbine list such as throwing them into another list, 
+    /// or fetching a turbine based on other factors like operating state, etc.
+    /// </summary>
     class TurbineFactory
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(TurbineFactory));
+        private List<Turbine> turbineList;
+
+        public TurbineFactory(List<string> turbinePrefixList)
+        {
+            turbineList = new List<Turbine>();
+
+        }
+
+        /// <summary>
+        /// Create the turbines given a list of prefixes. 
+        /// 
+        /// SHOULD ONLY BE CALLED ONCE
+        /// </summary>
+        public void createTurbines()
+        {
+            log.Info("Creating turbine lists");
+            throw new NotImplementedException();
+        }
+
+        public void pauseTurbine(Turbine turbine)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void addTurbineToList(Turbine turbine)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Method to get a turbine object given a turbine prefix
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        public Turbine getTurbine(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Method to get turbine object given a turbine 
+        /// </summary>
+        /// <param name="turbine"></param>
+        /// <returns></returns>
+        public Turbine getTurbine(Turbine turbine)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the list of turbines for the site
+        /// </summary>
+        /// <returns></returns>
+        public List<Turbine> getTurbineList()
+        {
+            return this.turbineList;
+        }
+
     }
 }
