843f4ee92217fad505ee21893688a46a105ba8e9
Stephen <cheunste@gmail.com>
2019-05-31 09:06:37 -0700

    Rearranged the methods in Turbine class somewhat.
    Also moved the temperatureQueeu to the main Articuno class.

--- Articuno/Turbine.cs
+++ Articuno/Turbine.cs
@@ -12,6 +12,10 @@ namespace Articuno
     /// The turbine class exposes OPC tags that are relevant to the program.
     /// You can set the tags via the setXXXXXTag method and get the value of said tag via the getXXXXXXValue method
     /// </summary>
+
+    //IMPORTANT: Getters and Setters MUST specify whether they're SETting the OPC Tag name to the field or SETting the OPC Value
+    // Getters don't really have this problem as it just GETs the OPC Value. 
+
     class Turbine
     {
 
@@ -25,6 +29,8 @@ namespace Articuno
         private string primMetReference;
         private string secMetReference;
         private string loadShutDown;
+        private string turbineCtrTag;
+        private string turbineScalingFactor;
 
         //Member variables for algorithm
         private bool temperatureConditionMet;
@@ -33,11 +39,11 @@ namespace Articuno
         private bool turbinePerformanceConditionMet;
         private bool derateConditionMet;
 
-        private int currentTurbSP;
-        private Queue temperatureQueue;
+        //Other fields
+        //scaling factor for turbine
+        private int currentTurbSF;
+        //this determines if the turbine is participating in Articuno or not. This must be a 'high priority check'  
         private bool articunoParicipation;
-
-
 
         //Log
         private static readonly ILog log = LogManager.GetLogger(typeof(Setup));
@@ -50,25 +56,42 @@ namespace Articuno
         //Detects when operating state (run, pause, etc.) changes
         public string operatingStateChanged() { throw new NotImplementedException(); }
 
-        //Get the value for the wind speed, rotor speed, etc. value from the OPC Server
+        //Getters to get the value for the wind speed, rotor speed, etc. value from the OPC Server
         public double getWindSpeedValue(OpcServer server) { throw new NotImplementedException(); }
         public double getrotorSpeedValue(OpcServer server) { throw new NotImplementedException(); }
         public double getOperatinStateValue(OpcServer server) { throw new NotImplementedException(); }
         public double getNrsStateValue(OpcServer server) { throw new NotImplementedException(); }
         public double getTemperatureValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getTurbineCtrValue(OpcServer server) {  throw new NotImplementedException(); }
+        public double getTurbineTemperatureValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getTurbineHumidityValue(OpcServer server) { throw new NotImplementedException(); }
+        public double getTurbineSFValue() {throw new NotImplementedException(); }
+
         //Load shutdown is a bit different as it is a command
         public double sentLoadShutdownCmd(OpcServer server) {
             log.Info("Shutdown command for "+this.turbinePrefix + " has been sent");
             throw new NotImplementedException();
         }
 
-        //Sets the OPC tag to the class's  member variable
+        //Setters to set the member variables to the  OPC tag
+        // THESE ARE NOT USED TO SET OPC VALUE
         public void setWindSpeedTag(string tag) { throw new NotImplementedException(); }
         public void setrotorSpeedTag(string tag) { throw new NotImplementedException(); }
         public void setOperatinStateTag(string tag) { throw new NotImplementedException(); }
         public void setNrsStateTag(string tag) { throw new NotImplementedException(); }
         public void setTemperatureTag(string tag) { throw new NotImplementedException(); }
         public void setLoadShutdownTag(string tag) { throw new NotImplementedException(); }
+        public void setTurbineCtrTag(string tag) { throw new NotImplementedException(); }
+        public void setTurbineTemperatureTag(string tag) { throw new NotImplementedException(); }
+        public void setTurbineHumidityTag(string tag) { throw new NotImplementedException(); }
+
+        //The setters to set the OPC Tag Values. There shouldn't be too many of these
+        public string setTurbineCtrValue() {   throw new NotImplementedException(); }
+        public void setTurbineSFValue(int scalingFactor) { this.currentTurbSF = scalingFactor; }
+        //Misc functions
+        public string getTurbinePrefixValue() { return this.turbinePrefix; }
+        public string isDerated() { return this.deRate; }
+
 
         //The following five fucntions are set by the main Articuno class. They show if each of the four/five 
         //algorithms are true
@@ -78,32 +101,10 @@ namespace Articuno
         public void setTurbinePerformanceCondition(bool state) { turbinePerformanceConditionMet = state; }
         public void setDeRateCondition(bool state) { derateConditionMet = state; }
         
-        //Misc functions
-        public string getTurbinePrefix() { return this.turbinePrefix; }
-
-        public string getTurbineCTR() {   throw new NotImplementedException(); }
 
         //Met Tower References for turbines. One sets the met tower reference (upon create) and the other gets it. 
         //These can be set to another reference  if/when they fail
         public void setMetReference() {    throw new NotImplementedException(); }
         public int getMetReference() {    throw new NotImplementedException(); }
-
-        //Turbine set point getter and setter
-        public int getTurbineSP(){throw new NotImplementedException(); }
-        public void setTurbineSP(int setPoint){throw new NotImplementedException(); }
-
-        /*
-        This method is special. It adds the current temperature to a temperature queue. 
-        This method is also in the main Articuno class and
-        This is used for calculating CTR minute temperature averages based on averaging one minute averages, which CORE may or may not be providing
-        IF AND ONLY IF The met tower temperatures are NOT working. This is a used for turbines that are supposed to be met tower redundancy
-        The way it works is as follows
-         - you collect one minute average temperatures values every minute (This will be determined by your timer class)
-         - After CTR minute has passed (this could be 10, or 15, or 1, it is dependent on user), then 
-         - calculate the average temperature of all the one min temperature values in the entire queue
-         - perform a dequeue to remove the first most value
-        */
-        public void addToTemperatureQueue(double temperature) { throw new NotImplementedException(); } 
-
     }
 }
