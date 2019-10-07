9b6af20c9acab9a65777e39e3cf42dac78de771e
Stephen <cheunste@gmail.com>
2019-10-07 06:42:18 -0700

    Added the CtrTemperature, CtrDew and CtrHumidity calls in Articuno/MetTower and MetTowerMediator. This will be how the dispatchers see the current temp, dew and humidity from Articuno

--- Articuno/MetTowerMediator.cs
+++ Articuno/MetTowerMediator.cs
@@ -108,6 +108,9 @@ namespace Articuno
                 met.HumidityBadQualityTag = reader.Rows[0]["HumidityBadQualityTag"].ToString();
                 met.IceIndicationTag = reader.Rows[0]["IceIndicationTag"].ToString();
                 met.NoDataAlarmTag = reader.Rows[0]["NoDataAlarmTag"].ToString();
+                met.CtrTemperatureTag = reader.Rows[0]["CtrTemperature"].ToString();
+                met.CtrDewTag = reader.Rows[0]["CtrDew"].ToString();
+                met.CtrHumidityTag = reader.Rows[0]["CtrHumidity"].ToString();
 
                 metTowerList.Add(met);
             }
@@ -213,22 +216,38 @@ namespace Articuno
 
         internal double calculateCtrAvgTemperature(string metId)
         {
-            Queue<double> tempQueue = getMetTower(metId).getTemperatureQueue();
+            MetTower met = getMetTower(metId);
+            Queue<double> tempQueue = met.getTemperatureQueue();
             double temperatureCtrAverage = 0.0;
             double count = tempQueue.Count;
             while (tempQueue.Count != 0) { temperatureCtrAverage += tempQueue.Dequeue(); }
-            return temperatureCtrAverage / count;
+
+            double average =temperatureCtrAverage/count;
+            met.CtrTemperatureValue = average;
+
+            return average;
 
         }
 
         internal double calculateCtrAvgHumidity(string metId)
         {
-            Queue<double> humidityQueue = getMetTower(metId).getHumidityQueue();
+            MetTower met = getMetTower(metId);
+            Queue<double> humidityQueue = met.getHumidityQueue();
             double humidityCtrAverage = 0.0;
             double count = humidityQueue.Count;
             while (humidityQueue.Count != 0) { humidityCtrAverage += humidityQueue.Dequeue(); }
-            return humidityCtrAverage / count;
+
+            double average = humidityCtrAverage / count;
+            met.CtrHumidityValue = average;
+            return average;
         }
+
+        internal void updateDewPoint(string metId,double ctrTemperature, double ctrHumidity)
+        {
+            double dew=calculateDewPoint(ctrHumidity, ctrTemperature);
+            getMetTower(metId).CtrDewValue = dew;
+        }
+
 
         /// <summary>
         /// Write Temperature Threshold for all the met tower
