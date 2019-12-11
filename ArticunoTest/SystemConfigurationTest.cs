f65e29a04099b2e1bb63d62a05ca3c7e6c65d59b
Stephen <cheunste@gmail.com>
2019-12-11 14:01:40 -0800

    Created a new test, system configuration tst to verify the opc tag and values in the system configuration tables

---
+++ ArticunoTest/SystemConfigurationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArticunoTest
+{
+    [TestClass]
+    public class SystemConfigurationTest
+    {
+        [TestMethod]
+        public void getTemperatreThresholdOpcTag() { }
+
+        [TestMethod]
+        public void getArticunoEnableOpcTag() { }
+
+        [TestMethod]
+        public void getCtrPeriodOpcTag() { }
+        [TestMethod]
+        public void getScalingFactorOpcTag() { }
+        [TestMethod]
+        public void getOpcServerNameTag() { }
+        [TestMethod]
+        public void getHeartbeatIntervalValue() {  }
+        [TestMethod]
+        public void getTurbineStartupTimeValue() { }
+        [TestMethod]
+        public void getFlatlineSamplesValue() { }
+        [TestMethod]
+        public void getMetTowerCtrCountDownTag()
+        {
+            Assert.Fail("Met Count Samples not implemented Test ");
+
+        }
+
+    }
+}
