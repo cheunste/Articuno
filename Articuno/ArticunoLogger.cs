1652eec0c3c790b3a1a2acf2a8fb6ff828577451
Stephen <cheunste@gmail.com>
2020-11-03 15:24:18 -0800

    Added NLog as a package

---
+++ Articuno/ArticunoLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articuno {
+    internal static class ArticunoLogger {
+        internal static readonly NLog.Logger GeneralLogger = NLog.LogManager.GetLogger("General");
+        internal static readonly NLog.Logger CurtailmentLogger = NLog.LogManager.GetLogger("Curtailment");
+        internal static readonly NLog.Logger DataLogger = NLog.LogManager.GetLogger("Data");
+    }
+}
